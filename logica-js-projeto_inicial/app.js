2a6818dcb80fc5b14215ef72e4b607b4c07c812e
joaovitorodev <joaovitor.odev@gmail.com>
2026-05-14 15:36:38 -0300

    exercicio mais conteudo da aula

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,29 @@
+/*alert("Boas vindas ao jogo do número secreto");
+
+let num = 10;
+
+let chute = prompt("Diga um número entre 1 e 100: ");
+
+if (num == chute) {
+    console.log("Você acertou o número");
+} else {
+    console.log("Você errou o número secreto, tente de novo");
+}*/
+
+/*alert("Boas Vindas ao Nosso Site!");
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 100;
+
+let mensagemDeErro = alert("Preencha todos os campos");
+
+let perguntaIdade = prompt("Qual a sua idade?");
+
+if (perguntaIdade < 18) {
+    alert("Você é menor de idade");
+} else {
+    alert("Você é maior de idade");
+}*/
+
