853fa60b4642b7f52722da93af635aacd44183fd
joaovitorodev <joaovitor.odev@gmail.com>
2026-05-15 19:42:12 -0300

    mudanças realizadas

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -2,13 +2,15 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute;
+let tentativas = 1;
+
 //enquanto
 while (chute != numeroSecreto) {
     chute = prompt("Escolha um número entre 1 e 10");
     
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
     } else {
         //alert('Você errou :(')
     if (numeroSecreto > chute) {
@@ -16,6 +18,7 @@ while (chute != numeroSecreto) {
     } else {
         alert("O número secreto é menor que " + chute);
     }
+    tentativas++;
   }
 }
 
