89c6cca2494b1b1f79c8d45eda12939a6659f2ae
joaovitorodev <joaovitor.odev@gmail.com>
2026-05-15 20:52:44 -0300

    mudança no número máximo

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,15 @@
-/*
+
 //jogo do número numero secreto
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
        break;
@@ -26,6 +27,7 @@ while (chute != numeroSecreto) {
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
 
+/*
 //atividade prática 1
 //1.Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
 alert("Boas Vindas ao Nosso Site!");
