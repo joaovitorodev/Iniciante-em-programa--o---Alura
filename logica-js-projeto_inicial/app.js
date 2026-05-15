9d598cee5545b11364f92dacc7438bb42311ec05
joaovitorodev <joaovitor.odev@gmail.com>
2026-05-15 20:06:08 -0300

    jogo numero secreto completo

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,16 +1,31 @@
-/*//jogo do número secreto
-alert("Boas vindas ao jogo do número secreto");
+//jogo do número numero secreto
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto)
+let chute;
+let tentativas = 1;
 
-let num = 10;
-
-let chute = prompt("Diga um número entre 1 e 100: ");
-
-if (num == chute) {
-    console.log("Você acertou o número");
-} else {
-    console.log("Você errou o número secreto, tente de novo");
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        // tentativas = tentativas + 1
+        tentativas++
+    }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+/*
 //atividade prática 1
 //1.Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
 alert("Boas Vindas ao Nosso Site!");
